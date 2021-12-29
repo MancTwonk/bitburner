664f5f491bf0074931d9e0455418fa8d8d6a3d73
MancTwonk <mark@mandmsoftware.co.uk>
2021-12-29 19:51:12 +0000

    add hacking script

---
+++ hacks/hack.ts
@@ -0,0 +1,17 @@
+import { NS } from '../index';
+
+/** @param {NS} ns */
+export default async function main(ns: NS) {
+  const target = ns.args[0].toString() || null;
+  const threads = Number(ns.args[1]) || 1;
+  const delay = Number(ns.args[2]) || 0;
+  const messWithStocks = Boolean(ns.args[3]) || false;
+
+  if (delay && delay > 0) {
+    await ns.sleep(delay);
+  }
+
+  ns.print(`Hacking ${target} using ${threads} threads`);
+  await ns.hack(target, { threads, stock: messWithStocks });
+  ns.exit();
+}
