ac43891e7f7f57d3b0bb3fcc3980a65389f0ee9a
MancTwonk <mark@mandmsoftware.co.uk>
2021-12-29 19:40:49 +0000

    add weaken script

---
+++ hacks/weaken.ts
@@ -0,0 +1,22 @@
+import { NS } from '../index';
+
+/** @param {NS} ns */
+export default async function main(ns: NS) {
+  const target = ns.args[0].toString() || null;
+  const threads = Number(ns.args[1]) || 1;
+  const delay = Number(ns.args[2]) || 0;
+  const messWithStocks = Boolean(ns.args[3]) || false;
+
+  if (!target) {
+    ns.print(`${target} is not a valid target`);
+    ns.exit();
+  }
+
+  if (delay && delay > 0) {
+    await ns.sleep(Number(delay));
+  }
+
+  ns.print(`Weakening ${target} using ${threads} threads`);
+  await ns.weaken(target, { threads, stock: messWithStocks });
+  ns.exit();
+}
